ef795cf328f582418fee3834adfcefce8ff764f0
JunoDash <jperezreyes@cps.edu>
2025-12-01 14:32:29 +0000

    did some work

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -16,30 +16,53 @@
 
 # List comprehensions provide a concise way to create lists.
 
+# fruits =    ["apple", "orange", "banana", "coconut"] # row 0
+# vegtables = ["celary", "carrots", "potatoes"] 
+# meats =     ["chicken", "fish", "turkey"]
+
+# grocieries = [fruits, vegtables, meats]
+# print(grocieries[1][2]) # first is down, next is verticle
+
+grocieries = [["apple", "orange", "banana", "coconut"], 
+              ["celary", "carrots", "potatoes"], 
+              ["chicken", "fish", "turkey"]]
+
+for collection in grocieries:
+    for food in collection:
+        print(food)
+
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
 # Examples:
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
 
-print(matrix[1][2])    # 6
+# print(matrix[1][2])    # 6
 
-# List comprehension
-first_col = [row[0] for row in matrix]
-print(first_col)       # [1, 4, 7]
+# # List comprehension
+# first_col = [row[0] for row in matrix]
+# print(first_col)       # [1, 4, 7]
 
 
 
-# Practice Problems:
+# # Practice Problems:
 
-# Build a matrix variable containing 3 lists of 3 numbers each.
+# # Build a matrix variable containing 3 lists of 3 numbers each.
 
-# Print the first list.
+# # Print the first list.
 
-# Print the second item from the third list.
+# # Print the second item from the third list.
 
-# Use a list comprehension to extract the last item from each sub-list.
+# # Use a list comprehension to extract the last item from each sub-list.
 
-# Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
+# # Challenge: Create a new list containing squares of numbers from 1–10 using a comprehension.
